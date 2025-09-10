8843a3a162a4797a89afc62b9c8af5a8c8456241
JP <jp@jp.nl>
2025-09-10 11:47:19 +0200

    BackgroundWorker laten zien

--- demo/DemoSolution/WpfApp/MainWindow.xaml.cs
+++ demo/DemoSolution/WpfApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -37,5 +38,28 @@ public partial class MainWindow : Window
 			lblResult.Dispatcher.Invoke(() => lblResult.Content = "klaar!");
 		});
 		t.Start();
+	}
+
+	private void ButtonWorkerBase_OnClick(object sender, RoutedEventArgs e)
+	{
+		var worker = new BackgroundWorker();
+		worker.WorkerReportsProgress = true;
+		worker.ProgressChanged += (o, args) => lblResultWorker.Content = $"Voortgang! {args.ProgressPercentage}%";
+		worker.RunWorkerCompleted += (o, args) => lblResultWorker.Content = $"Klaar! {args.Result}";
+		worker.DoWork += (o, args) =>
+		{
+			worker.ReportProgress(20);
+			Thread.Sleep(200);
+			worker.ReportProgress(40);
+			Thread.Sleep(2000);
+			worker.ReportProgress(65);
+			Thread.Sleep(800);
+			worker.ReportProgress(85);
+			Thread.Sleep(1300);
+			worker.ReportProgress(100);
+			Thread.Sleep(5000);
+			args.Result = 42;
+		};
+		worker.RunWorkerAsync();
 	}
 }
