aef0bba2d8f5969ac3db8a61b21c5896f01cb815
JP <jp@jp.nl>
2025-09-10 15:44:54 +0200

    PLINQ en Parallel

---
+++ demo/DemoSolution/TaskProject/Plinq.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace TaskProject;
+
+public class Plinq
+{
+	public static void Doe()
+	{
+		List<int> lijstje = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+
+		var watch = new Stopwatch();
+		watch.Start();
+		var filtered = lijstje.AsParallel().Where(x => HeleZwareFilterOperatie(x)).ToArray();
+		watch.Stop();
+		foreach (var item in filtered)
+		{
+			Console.WriteLine($"item: {item}");
+		}
+
+		Console.WriteLine($"Elapsed milliseconds: {watch.ElapsedMilliseconds}");
+	}
+
+	public static bool HeleZwareFilterOperatie(int item)
+	{
+		Console.WriteLine($"berekenen voor item {item} / {Thread.CurrentThread.ManagedThreadId}");
+		Thread.SpinWait(50_000_000);
+		return true;
+	}
+}
--- demo/DemoSolution/TaskProject/Program.cs
+++ demo/DemoSolution/TaskProject/Program.cs
@@ -5,15 +5,46 @@ using TaskProject;
 // BasicTasks.Doe();
 // Canceling.Doe();
 
-try
+// Plinq.Doe();
+
+// Task.WaitAl
+
+// try
+// {
+// 	Parallel.Invoke(
+// 		() => { throw new NotImplementedException("Ga weg"); },
+// 		() => { throw new NotSupportedException("Ga weg"); },
+// 		() => { throw new ArgumentOutOfRangeException("Ga weg"); }
+// 	);
+// }
+// catch (AggregateException ex)
+// {
+// 	
+// }
+
+Parallel.For(1, 100, (index, loopState) =>
 {
-	Parallel.Invoke(
-		() => { throw new NotImplementedException("Ga weg"); },
-		() => { throw new NotSupportedException("Ga weg"); },
-		() => { throw new ArgumentOutOfRangeException("Ga weg"); }
-	);
-}
-catch (AggregateException ex)
-{
+	if (index == 4)
+	{
+		Console.WriteLine("breaking want 4!");
+		loopState.Break();
+	}
 	
-}
+	Thread.Sleep(100 * Random.Shared.Next(1, 50));
+	Console.WriteLine($"i: {index}");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
